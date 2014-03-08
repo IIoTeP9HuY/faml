9d47a1f321954887c8100f23ac38a6db1d21f1d4
acid <acid@yandex-team.ru>
2014-03-08 19:12:27 +0400

    core: distances functions vectorized

--- include/faml/distances.hpp
+++ include/faml/distances.hpp
@@ -23,7 +23,7 @@ struct DistanceFunction {
 
 template<typename T>
 struct MinkowskiDistance : DistanceFunction<T> {
-	MinkowskiDistance(int power): power(power) {}
+	MinkowskiDistance(size_t power): power(power) {}
 
 	double operator ()(const T &lhs, const T &rhs) const {
 		size_t N = lhs.size();
@@ -31,16 +31,11 @@ struct MinkowskiDistance : DistanceFunction<T> {
 			throw std::invalid_argument("Arguments should have same length");
 		}
 
-		double distance = 0;
-
-		for (size_t i = 0; i < N; ++i) {
-			distance += fastpow(fabs(rhs[i] - lhs[i]), power);
-		}
-
+		double distance = (rhs - lhs).cwiseAbs().array().pow(power).sum();
 		return pow(distance, 1.0 / power);
 	}
 
-	int power;
+	size_t power;
 };
 
 template<typename T>
@@ -53,17 +48,11 @@ struct CosineDistance : DistanceFunction<T> {
 			throw std::invalid_argument("Arguments should have same length");
 		}
 
-		double scalarProduct = 0;
-		double lhsNorm = 0;
-		double rhsNorm = 0;
+		double scalarProduct = lhs.dot(rhs);
+		double lhsNorm = lhs.norm();
+		double rhsNorm = rhs.norm();
 
-		for (size_t i = 0; i < N; ++i) {
-			scalarProduct += lhs[i] * rhs[i];
-			lhsNorm += lhs[i] * lhs[i];
-			rhsNorm += rhs[i] * rhs[i];
-		}
-
-		double distance = 1 - scalarProduct / (sqrt(lhsNorm) * sqrt(rhsNorm));
+		double distance = 1 - scalarProduct / (lhsNorm * rhsNorm);
 
 		return distance;
 	}
@@ -79,15 +68,9 @@ struct OverlapDistance : DistanceFunction<T> {
 			throw std::invalid_argument("Arguments should have same length");
 		}
 
-		double mutualIntensity = 0;
-		double lhsNorm = 0;
-		double rhsNorm = 0;
-
-		for (size_t i = 0; i < N; ++i) {
-			mutualIntensity += std::min(lhs[i], rhs[i]);
-			lhsNorm += lhs[i];
-			rhsNorm += rhs[i];
-		}
+		double mutualIntensity = lhs.cwiseMin(rhs).sum();
+		double lhsNorm = lhs.sum();
+		double rhsNorm = rhs.sum();
 
 		double distance = 1 - 2 * mutualIntensity / (lhsNorm + rhsNorm);
 
@@ -105,11 +88,13 @@ struct WMinkowskiDistance : DistanceFunction<T> {
 			throw std::invalid_argument("Arguments should have same length");
 		}
 
-		double distance = 0;
+		double distance = (weights.array() * (rhs - lhs).cwiseAbs().array().pow(power)).sum();
 
-		for (size_t i = 0; i < N; ++i) {
-			distance += weights(i) * fastpow(fabs(rhs[i] - lhs[i]), power);
-		}
+//		double distance = 0;
+
+//		for (size_t i = 0; i < N; ++i) {
+//			distance += weights(i) * fastpow(fabs(rhs[i] - lhs[i]), power);
+//		}
 
 		return pow(distance, 1.0 / power);
 	}
