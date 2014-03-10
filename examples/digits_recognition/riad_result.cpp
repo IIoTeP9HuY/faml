e8d98b81574560bc797fe29f222142b75f1acdcc
Alexey Dmitriev <github@admitriev.name>
2014-03-10 12:23:38 +0400

    riad_result + k=19

--- examples/digits_recognition/riad_result.cpp
+++ examples/digits_recognition/riad_result.cpp
@@ -21,6 +21,7 @@ using namespace Eigen;
 
 int main() {
 	auto testData = readCSV("mnist_small_train.csv");
+	auto testStr = readCSV("mnist_test_no_label.csv");
 	typedef std::vector<std::string> StrRowType;
 	typedef VectorXf SampleType;
 	typedef unsigned long long Label;
@@ -37,59 +38,57 @@ int main() {
 			return std::stod(x);
 		}
 	);
+
+	auto test = testStr.castByElement<VectorXf>(
+		[](const std::string& x) {
+			return std::stod(x);
+		}
+	);
 	trainXstr.clear();
 	trainYstr.clear();
 
-	auto columns = trainX.getColumnsNames();
-
-	mt19937 gen(1993);
-
-	auto indicies = trainTestSplit(trainX.rowsNumber(), (size_t)19000, gen);
-	auto subtrainX = trainX[indicies.first];
-	auto subtrainY = trainY[indicies.first];
-	auto subtestX = trainX[indicies.second];
-	auto subtestY = trainY[indicies.second];
-
-	trainX.clear();
-	trainY.clear();
-
 	std::vector<std::unique_ptr<Scaler<VectorXf>>> scalers;
 	scalers.emplace_back(new DummyScaler<VectorXf>());
+//	scalers.emplace_back(new PowerAmplifyScaler(1.4));
 	//scalers.emplace_back(new NormalScaler());
 	//scalers.emplace_back(new MinMaxScaler(columns.size(), 0, 1));
 
 	std::vector<std::unique_ptr<KernelFunction>> kernels;
 	kernels.emplace_back(new QuarticKernel());
-	//kernels.emplace_back(new DiscreteKernel());
-	//kernels.emplace_back(new InverseKernel());
+	//	kernels.emplace_back(new DiscreteKernel());
+	// kernels.emplace_back(new InverseKernel());
 	//kernels.emplace_back(new RBFKernel(1.0));
 	//kernels.emplace_back(new EpanechnikovKernel());
 
 	std::vector<std::unique_ptr<DistanceFunction<VectorXf>>> distances;
-	//distances.emplace_back(new EuclidianDistance());
-	//distances.emplace_back(new MinkowskiDistance(3));
-	//distances.emplace_back(new MinkowskiDistance(5));
+//	distances.emplace_back(new EuclidianDistance());
+//	distances.emplace_back(new MinkowskiDistance(3));
+//	distances.emplace_back(new MinkowskiDistance(5));
 	distances.emplace_back(new CosineDistance());
-	//distances.emplace_back(new OverlapDistance());
+//	distances.emplace_back(new OverlapDistance());
 
 	for(const auto& scaler: scalers) {
-		scaler->train(subtrainX);
+		scaler->train(trainX);
 		auto lambda = [&scaler](const VectorXf& row) {
 			return (*scaler)(row);
 		};
-		auto scaledX = subtrainX.cast(lambda);
-		auto scaledTest = subtestX.cast(lambda);
-		for(size_t k = 17; k <= 19; ++k) {
+		auto scaledX = trainX.cast(lambda);
+		auto scaledTest = test.cast(lambda);
+		for(size_t k = 19; k <= 19; ++k) {
 			for(const auto& distance: distances) {
 				for(const auto& kernel: kernels) {
 					clock_t start = clock();
 					KNNClassifier<VectorXf, Label> knn(k, *distance, *kernel);
-					knn.train(scaledX, subtrainY);
+					knn.train(scaledX, trainY);
 					auto prediction = knn.predict(scaledTest);
-					cout << scaler->toString() << ' ' << distance->toString() << ' ' << kernel->toString() << "\n";
-					cout << "Score: " << accuracyScore(subtestY, prediction) << "\n";
-					cout << "time " << (clock() - start) / 1.0 / CLOCKS_PER_SEC;
-					cout << endl;
+					cout << "Id,Prediction" << "\n";
+					for(size_t i = 0; i < prediction.rowsNumber(); ++i) {
+						cout << i + 1 << "," << prediction[i] << "\n";
+					}
+//					cout << k << ' ' << scaler->toString() << ' ' << distance->toString() << ' ' << kernel->toString() << "\n";
+//				cout << "Score: " << accuracyScore(subtestY, prediction) << "\n";
+//					cout << "time " << (clock() - start) / 1.0 / CLOCKS_PER_SEC;
+//					cout << endl;
 				}
 			}
 		}
