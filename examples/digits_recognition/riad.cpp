e7989fc49ed60d3df2a6f215fb281cbf3e4f6326
Alexey Dmitriev <github@admitriev.name>
2014-03-30 23:47:41 +0400

    examples: riad: remove unused code in comments

--- examples/digits_recognition/riad.cpp
+++ examples/digits_recognition/riad.cpp
@@ -59,50 +59,6 @@ int main() {
 	cerr << "before preprocessing" << endl;
 	int size = 28;
 
-
-	/*auto trainX = rtrainX.cast(
-		[](const VectorXf& v) {
-			VectorXf res = VectorXf::Zero(14 * 14);
-			for(int i = 0; i < 14; ++i) {
-				for(int j = 0; j < 14; ++j) {
-					for(int ii = 0; ii < 2; ++ii) {
-						for(int jj = 0; jj < 2; ++jj) {
-							res[i * 14 + j] += v[(2 * i + ii) * 28 + 2 * j + jj];
-						}
-					}
-				}
-			}
-			return res;
-		}
-	);
-	 
-	size /= 2;*/
-
-/*	trainX = rtrainX.cast(
-		[size](const VectorXf& vv) {
-			VectorXf res = VectorXf::Zero(size * size);
-
-			for(int i = 0; i < size; ++i) {
-				for(int j = 0; j < size; ++j) {
-					int cnt = 0;
-					for(int ii = -1; ii <= 1; ++ii) {
-						for(int jj = -1; jj <= 1; ++jj){
-							if(i + ii < 0 || i + ii >= size) {
-								continue;
-							}
-							if(j + jj < 0 || j + jj >= size) {
-								continue;
-							}
-							++cnt;
-							res[i * size + j] += vv[(i + ii) * size + j + jj];
-						}
-					}
-					res[i * size + j] /= cnt;
-				}
-			}
-			return res;
-		}
-	);*/
 	trainX = trainX.cast(
 		[size](const VectorXf& vv) {
 			VectorXf res = VectorXf::Zero(size * size);
@@ -145,39 +101,23 @@ int main() {
 
 	std::vector<std::unique_ptr<KernelFunction>> kernels;
 	kernels.emplace_back(new QuarticKernel());
-	//kernels.emplace_back(new DiscreteKernel());
-	//kernels.emplace_back(new InverseKernel());
-	//kernels.emplace_back(new RBFKernel(1.0));
-	//kernels.emplace_back(new EpanechnikovKernel());
 
 	std::vector<std::unique_ptr<DistanceFunction<VectorXf>>> distances;
-	//distances.emplace_back(new EuclidianDistance());
-	//distances.emplace_back(new MinkowskiDistance(3));
-	//distances.emplace_back(new MinkowskiDistance(5));
 	distances.emplace_back(new CosineDistance());
-	//distances.emplace_back(new OverlapDistance());
 
-//	for(const auto& scaler: scalers) {
-//		scaler->train(subtrainX);
-//		auto lambda = [&scaler](const VectorXf& row) {
-//			return (*scaler)(row);
-//		};
-//		auto scaledX = subtrainX.cast(lambda);
-//		auto scaledTest = subtestX.cast(lambda);
-		for(size_t k = 1; k <= 20; ++k) {
-			for(const auto& distance: distances) {
-				for(const auto& kernel: kernels) {
-					clock_t start = clock();
-					KNNClassifier<VectorXf, Label> knn(k, *distance, *kernel);
-					double res = crossValidate(knn, subtrainX, subtrainY, KFold(subtrainX.rowsNumber(), 5), AccuracyScorer<Label>());
-					cout << k << ' ' <<distance->toString() << ' ' << kernel->toString() << "\n";
-					cout << "Score: " << res << "\n";
-					cout << "time " << (clock() - start) / 1.0 / CLOCKS_PER_SEC;
-					cout << endl;
-				}
+	for(size_t k = 1; k <= 20; ++k) {
+		for(const auto& distance: distances) {
+			for(const auto& kernel: kernels) {
+				clock_t start = clock();
+				KNNClassifier<VectorXf, Label> knn(k, *distance, *kernel);
+				double res = crossValidate(knn, subtrainX, subtrainY, KFold(subtrainX.rowsNumber(), 5), AccuracyScorer<Label>());
+				cout << k << ' ' <<distance->toString() << ' ' << kernel->toString() << "\n";
+				cout << "Score: " << res << "\n";
+				cout << "time " << (clock() - start) / 1.0 / CLOCKS_PER_SEC;
+				cout << endl;
 			}
 		}
-//	}
+	}
 
 	return 0;
 }
